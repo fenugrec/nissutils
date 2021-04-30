f4e2e7bea21ea05c6b5bc57686c6a88d69be752d
fenugrec <fenugrec@users.sourceforge.net>
2021-04-30 19:48:14 -0400

    add findrefs test script

---
+++ tests/test_findrefs1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+./test_findrefs ../tests/testrom.bin 0x6000
+./test_findrefs ../tests/testrom.bin 0x6001
+./test_findrefs ../tests/testrom.bin 0x6002
+./test_findrefs ../tests/testrom.bin 0x6003
+./test_findrefs ../tests/testrom.bin 0x6004
+./test_findrefs ../tests/testrom.bin 0x6005
+./test_findrefs ../tests/testrom.bin 0x6006
+./test_findrefs ../tests/testrom.bin 0x6007
+./test_findrefs ../tests/testrom.bin 0x6008
+./test_findrefs ../tests/testrom.bin 0x6009
+./test_findrefs ../tests/testrom.bin 0x600a
